cfa136d4de43da1c9900e60ceafc72b0472b595d
6upe <katongobupe444@gmail.com>
2023-05-02 19:31:01 -0700

    fixing routes 3

---
+++ api/procurement-adverts.js
@@ -0,0 +1,22 @@
+module.exports((req,res) => {
+
+    const blogs = [
+        {
+          blogID: "1",
+          blogTitle: "My First Blog Post",
+          blogContent: "Lorem ipsum dolor sit amet...",
+        },
+        {
+          blogID: "2",
+          blogTitle: "My Second Blog Post",
+          blogContent: "Lorem ipsum dolor sit amet...",
+        },
+        {
+          blogID: "3",
+          blogTitle: "My Third Blog Post",
+          blogContent: "Lorem ipsum dolor sit amet...",
+        },
+      ];
+    
+      res.json(blogs);
+});
