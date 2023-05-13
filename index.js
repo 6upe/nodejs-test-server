b2e28750c48a81e9b77160dfe7a9a7d70e78837c
6upe <katongobupe444@gmail.com>
2023-05-13 00:19:12 -0700

    fixing cors: testing the API 2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-    origin: ['https://akebu6.github.io/College-UI/procurement.html'
+    origin: ['https://akebu6.github.io'
             , 'http://127.0.0.1:5502/'] // Replace with your allowed origin(s)
 
   }));
