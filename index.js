83dd307a001c559d19b09188b14d272f677f6872
6upe <katongobupe444@gmail.com>
2023-05-13 00:00:55 -0700

    fixing cors: added 2 origins

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+app.use(cors({
+    origin: ['https://akebu6.github.io/College-UI/procurement.html'
+            , 'http://127.0.0.1:5502/'] // Replace with your allowed origin(s)
+
+  }));
+  
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -20,6 +25,14 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/extractor', (req, res) => {
+    res.sendFile(__dirname + '/extractor.html');
+});
+
+app.get('/kwizme', (req, res) => {
+    res.sendFile(__dirname + '/kwizme.html');
 });
 
 app.get('/api/procurement-adverts', (req, res) => {
@@ -56,6 +69,49 @@ app.get('/api/procurement-adverts', (req, res) => {
 
     res.json(adverts);
 });
+
+const { pipeline } = require('@transformers/pipeline');
+const { Tokenizer } = require('@tokenizers/nltk');
+const { AutoModelForQuestionAnswering, AutoTokenizer } = require('@transformers/node');
+const { Tensor } = require('node-cuda-api').internal;
+// or const { Tensor } = require('node-cuda-api').internal;
+
+const model_name = 't5-base';
+const tokenizer = new Tokenizer({ modelType: model_name });
+const generator = pipeline('text2text-generation', { model: model_name, tokenizer });
+
+const qa_model_name = 'bert-large-uncased-whole-word-masking-finetuned-squad';
+const qa_tokenizer = AutoTokenizer.fromPretrained(qa_model_name);
+
+const qa_model = AutoModelForQuestionAnswering.fromPretrained(qa_model_name);
+
+
+app.post('/generate-quiz', async (req, res) => {
+    const { text } = req.body;
+
+    // Generate candidate questions using the language model
+    const questions = await generator(`generate question: ${text}`, { max_length: 128, num_return_sequences: 5 });
+    const candidateQuestions = questions.map(q => q.generated_text.replace('generate question:', '').trim());
+
+    // Extract potential answers using the question answering model
+    const answers = [];
+    for (const question of candidateQuestions) {
+        const inputs = qa_tokenizer.encodePlus(question, text);
+        const output = await qa_model(inputs.input_ids, inputs.attention_mask);
+        const start_logits = output.start_logits[0];
+        const end_logits = output.end_logits[0];
+        const all_tokens = qa_tokenizer.convertIdsToTokens(inputs.input_ids);
+        const answer = qa_tokenizer.decode(all_tokens.slice(start_logits, end_logits + 1)).trim();
+        answers.push(answer);
+    }
+
+    // Combine questions and answers to create quiz questions
+    const quizQuestions = candidateQuestions.map((question, i) => `What is ${answers[i]} in the context of: ${question}?`);
+
+    res.send(quizQuestions);
+});
+
+  
 
 const port = process.env.PORT || 4000;
 
